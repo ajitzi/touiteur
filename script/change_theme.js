5fd53bf75f962d5f799e20aadf12c0e530cc81e4
Ajitzi Carmona <ajitzi.carmona@outlook.com>
2021-04-30 16:45:41 +0200

    bouton thème

--- script/change_theme.js
+++ script/change_theme.js
@@ -1,16 +1,19 @@
 const btnTheme = document.querySelector("#btn-theme");
 const logo = document.querySelector("header img");
 const touit = document.querySelector(".touit button img");
+const light = document.querySelector("#btn-theme img");
 
 function changeTheme() {
     if (document.body.dataset.theme === "dark") {
         document.body.dataset.theme = ""
         logo.setAttribute("src", "img/logotouiteurV2.svg");
         touit.setAttribute("src", "img/touiteur_facesV2.svg");
+        light.setAttribute("src", "img/light-light.svg");
     } else {
         document.body.dataset.theme = "dark"
         logo.setAttribute("src", "img/logotouiteur-dark.svg");
         touit.setAttribute("src", "img/touiteur_faces-dark.svg");
+        light.setAttribute("src", "img/light-dark.svg");
     }
 }
 
